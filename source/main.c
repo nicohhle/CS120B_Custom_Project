85605bdd3494e528e70a65f550a81ffe44d5ece8
Nicole Nguyen <nnguy099@wch136-03.cs.ucr.edu>
2020-02-26 16:14:51 -0800

    started

--- source/main.c
+++ source/main.c
@@ -1,5 +1,5 @@
 /*	Author: nnguy099
- *  Partner(s) Name: Nicole Nguyen (nnguy099)
+ *  	Partner(s) Name:
  *	Lab Section:
  *	Assignment: Lab #  Exercise #
  *	Exercise Description: [optional - include for your own benefit]
@@ -12,12 +12,96 @@
 #include "simAVRHeader.h"
 #endif
 
+typedef enum boolean { true, false } boolean;
+
+///////////////////// TASK STRUCT /////////////////////
+
+typedef struct task {
+	int state;
+	unsigned long period;
+	unsigned long elapsedTime;
+	int (*TickFct)(int);
+} task;
+
+task task[1];
+
+const unsigned char tasksNum = 1;
+
+//////////////////// JOYSTICK ////////////////////
+
+unsigned short ADCValue;
+unsigned const short joyStickUpValue = 700; 
+unsigned const short joyStickDownValue = 300;
+
+void ADC_init() {
+	ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
+}
+
+boolean joyStickUp() {
+	ADCValue = ADC;
+	return (ADCValue > joyStickUpValue) ? true : false;
+}
+
+boolean joyStickDown() {
+	ADCValue = ADC;
+	return (ADCValue < joyStickDownValue) ? true : false;
+}
+
+///////////////////// LIGHTS /////////////////////
+
+enum TestState { start, up, down, wait, reset } TestState;
+
+int Test(int state) {
+	switch (state) { // transitions
+		case start: 
+			if (	
+	}
+
+	
+
+	switch(state) { // actions
+
+
+	}
+}
+
+///////////////////// INITIALIZE /////////////////////
+
+void Initialize() {
+	
+	unsigned char i = 0;
+	
+	tasks[i].period = 1;
+	tasks[i].elapsedTime = tasks[i].period;
+	tasks[i].state = start;
+	tasks[i].TickFct = &Test;
+
+}
+
+///////////////////// MAIN /////////////////////
+
 int main(void) {
-    /* Insert DDR and PORT initializations */
 
-    /* Insert your solution below */
+	DDRA = 0x00; PORTA = 0xFF; // Joystick (Input)
+	DDRC = 0x00; PORTC = 0xFF; // Button (Input)
+
+	DDRB = 0xFF; PORTB = 0x00; // Nokia 5110 (Output)
+	// DDRD = 0xFF; PORTD = 0x00; // LCD (Output)
+	
+	ADC_init();
+	PWM_on();
+
+	Initialize();
+
     while (1) {
-
+	unsigned char i;
+	for (i = 0; i < tasksNum; i++) {
+			if (tasks[i].elapsedTime >= tasks[i].period) {
+				tasks[i].state = tasks[i].TickFct(tasks[i].state);
+				tasks[i].elapsedTime = 0;
+			}
+			tasks[i].elapsedTime += 1;
+		}	
     }
     return 1;
 }
